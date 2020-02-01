1083935d409ad65ebc61df9eeb1c08a0993d533e
ijethwani <49839093+ijethwani@users.noreply.github.com>
2020-02-01 19:42:42 +0530

    setting with heroku

--- Blog_app/settings.py
+++ Blog_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -173,3 +174,5 @@ AUTHENTICATION_BACKENDS = (
 SITE_ID = 1
 
 #LOGIN_REDIRECT_URL = "/profile/"
+# Activate Django-Heroku.
+django_heroku.settings(locals())
